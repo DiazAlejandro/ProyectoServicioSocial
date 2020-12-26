bd7b172b45307233aa39edef752f548c2006e1a4
Alejandro Díaz Ruiz <56706986+DiazAlejandro@users.noreply.github.com>
2020-12-26 04:26:04 -0600

    Añadir Campos para documentos
    Se añaden campos del 1 al 7 para documentación. Restan otros 3

--- ServicioSocial/app.js
+++ ServicioSocial/app.js
@@ -70,9 +70,11 @@ function mostrarInformacion(){
                 var docEv = (snapshot.val() && snapshot.val().documentoEvidencia);
             var documentoEvidencia; //Variable que se mostrará en el HTML
             if (docEv == "Sin Añadir"){
-                documentoEvidencia = "Sin añadir";    //Esto aparece en HTML cuando no hay documento       
+                documentoEvidencia = "Sin añadir";    //Esto aparece en HTML cuando no hay documento   
+                document.getElementById('documentoEvidencia').style.display = 'inline';    
             }else{
                 documentoEvidencia = str.link(docEv); //.link convierte en enlace el dato recuperado de la BD
+                document.getElementById('documentoEvidencia').style.display = 'none'; 
             }
             //document.getElementById('idEtiquetaAmostrarEnHTML').innerHTML = varible;
             document.getElementById('nombre').innerHTML = nombre;
@@ -93,20 +95,88 @@ function mostrarElementos(){
     document.getElementById('accesoDocumentos').style.display = 'block';
 }
 
+
 function consultarDocumentos(){
     var userId = localStorage.getItem('id');
     firebase.database().ref('alumno/'+userId).once('value').then(function(snapshot){
+        //Operaciones para el manejo de la solicitud
         var strSol = "Solicitud Serv. Social";
-        var docSol = (snapshot.val() && snapshot.val().documentoSolitud);
+        var docSol = (snapshot.val() && snapshot.val().documentoSolicitud);
         var documentoSol; //Variable que se mostrará en el HTML
             if (docSol == "Sin Añadir" || docSol == "undefined"){
-                documentoSol = "Sin añadir";    //Esto aparece en HTML cuando no hay documento       
+                documentoSol = "Sin añadir";    //Esto aparece en HTML cuando no hay documento  
+                document.getElementById('inputSolicitud').style.display = 'inline';
             }else{
                 documentoSol = strSol.link(docSol); //.link convierte en enlace el dato recuperado de la BD
+                document.getElementById('inputSolicitud').style.display = 'none'; //Deshabilita input
             }
         document.getElementById('solicitud').innerHTML = documentoSol;
-    });
 
+        //Operaciones para el manejo de la Carta Compromiso
+        var strCarC = "Carta Compromiso";
+        var docCarC = (snapshot.val() && snapshot.val().documentoCartaCompromiso);
+        var documentoCartaC; //Variable que se mostrará en el HTML
+            if (docCarC == "Sin Añadir" || docCarC == "undefined"){
+                documentoCartaC = "Sin añadir";    //Esto aparece en HTML cuando no hay documento  
+                document.getElementById('inputCartaCom').style.display = 'inline';
+            }else{
+                documentoCartaC = strCarC.link(docCarC); //.link convierte en enlace el dato recuperado de la BD
+                document.getElementById('inputCartaCom').style.display = 'none'; //Deshabilita input
+            }
+        document.getElementById('cartaCompromiso').innerHTML = documentoCartaC;
+
+        //Operaciones para el manejo de la Tarjeta de Control
+        var strTarC = "Tarjeta de Control";
+        var docTarC = (snapshot.val() && snapshot.val().documentoTarjetaCtrl);
+        var documentoTarjetaC; //Variable que se mostrará en el HTML
+            if (docTarC == "Sin Añadir" || docTarC == "undefined"){
+                documentoTarjetaC = "Sin añadir";    //Esto aparece en HTML cuando no hay documento  
+                document.getElementById('inputTarjetaCtrl').style.display = 'inline';
+            }else{
+                documentoTarjetaC = strTarC.link(docTarC); //.link convierte en enlace el dato recuperado de la BD
+                document.getElementById('inputTarjetaCtrl').style.display = 'none'; //Deshabilita input
+            }
+        document.getElementById('tarjetaControl').innerHTML = documentoTarjetaC;
+
+        //Operaciones para el manejo de la Carta Asignacion
+        var strCartaAs = "Tarjeta de Control";
+        var docCarA = (snapshot.val() && snapshot.val().documentoCartaAsignacion);
+        var documentoCartaAs; //Variable que se mostrará en el HTML
+            if (docCarA == "Sin Añadir" || docCarA == "undefined"){
+                documentoCartaAs = "Sin añadir";    //Esto aparece en HTML cuando no hay documento  
+                document.getElementById('inputCartaAsig').style.display = 'inline';
+            }else{
+                documentoCartaAs = strCartaAs.link(docCarA); //.link convierte en enlace el dato recuperado de la BD
+                document.getElementById('inputCartaAsig').style.display = 'none'; //Deshabilita input
+            }
+        document.getElementById('cartaAsignacion').innerHTML = documentoCartaAs;
+
+        //Operaciones para el manejo de el Reporte Bimestral
+        var strRepB = "Reporte Bimestral";
+        var docRepB = (snapshot.val() && snapshot.val().documentoReporteB);
+        var documentoRepB; //Variable que se mostrará en el HTML
+            if (docRepB == "Sin Añadir" || docRepB == "undefined"){
+                documentoRepB = "Sin añadir";    //Esto aparece en HTML cuando no hay documento  
+                document.getElementById('inputReporteB').style.display = 'inline';
+            }else{
+                documentoRepB = strRepB.link(docRepB); //.link convierte en enlace el dato recuperado de la BD
+                document.getElementById('inputReporteB').style.display = 'none'; //Deshabilita input
+            }
+        document.getElementById('reporteBimestral').innerHTML = documentoRepB;
+
+        //Operaciones para el manejo de la Evaluacion
+        var strEva = "Formato Evaluación";
+        var docEva = (snapshot.val() && snapshot.val().documentoEvaluacion);
+        var documentoEva; //Variable que se mostrará en el HTML
+            if (docEva == "Sin Añadir" || docEva == "undefined"){
+                documentoEva = "Sin añadir";    //Esto aparece en HTML cuando no hay documento  
+                document.getElementById('inputReporteB').style.display = 'inline';
+            }else{
+                documentoEva = strEva.link(docEva); //.link convierte en enlace el dato recuperado de la BD
+                document.getElementById('inputReporteB').style.display = 'none'; //Deshabilita input
+            }
+        document.getElementById('formatoEvaluacion').innerHTML = documentoEva;
+    });
 }
 //Llamada automática, esta funcion permite que se ejecute dentro del 
 //archivo html sin necesidad de tener un evento de tipo onClick().
@@ -121,15 +191,17 @@ function actualizarEvidencia(){
     var userId    = localStorage.getItem('id'); //Recuperar el id del alumno
     var evidencia = document.getElementById('documentoEvidencia').value; //Guardar en una variable el enlace del alumno
     var validacion = "Pendiente de Revisión";
-    firebase.database().ref('alumno/'+userId).update({
-        documentoEvidencia:evidencia,
-        validacionEvidencia:validacion,
-    }).catch(function(error){
-        alert("Su documento no ha sido guardado con éxito. Intentelo mas tarde");
-    });
-    alert("Documento de Evidencia - Guardado");
-    location.reload(); //Actualizar la pagina automáticamente
-    document.getElementById("documentoEvidencia").value = ""; //Limpia el campo cada que se hace la operación
+    if (evidencia != ""){
+        firebase.database().ref('alumno/'+userId).update({
+            documentoEvidencia:evidencia,
+            validacionEvidencia:validacion,
+        }).catch(function(error){
+            alert("Su documento no ha sido guardado con éxito. Intentelo mas tarde");
+        });
+        alert("Documento de Evidencia - Guardado");
+        location.reload(); //Actualizar la pagina automáticamente
+        document.getElementById("documentoEvidencia").value = ""; //Limpia el campo cada que se hace la operación
+    }
 }
 
 function eliminarEvidencia(){
@@ -153,24 +225,171 @@ var notAdd = "Sin Añadir";
 function actualizarSolicitud(){
     var userId    = localStorage.getItem('id'); //Recuperar el id del alumno
     var solicitud = document.getElementById('inputSolicitud').value; //Guardar en una variable el enlace del alumno
-    firebase.database().ref('alumno/'+userId).update({
-        documentoSolitud:solicitud,
-    }).catch(function(error){
-        alert("Su documento no ha sido guardado con éxito. Intentelo mas tarde");
-    });
-    alert("Documento de Evidencia - Guardado");
-    location.reload(); //Actualizar la pagina automáticamente
-    document.getElementById('inputSolicitud').value = ""; //Limpia el campo cada que se hace la operación
+    if (solicitud != ""){
+        firebase.database().ref('alumno/'+userId).update({
+            documentoSolicitud:solicitud,
+        }).catch(function(error){
+            alert("Su documento no ha sido guardado con éxito. Intentelo mas tarde");
+        });
+        alert("Documento de Evidencia - Guardado");
+        location.reload(); //Actualizar la pagina automáticamente
+        document.getElementById('inputSolicitud').value = ""; //Limpia el campo cada que se hace la operación
+    }
 }
 
 function eliminarSolicitud(){
     var userId = localStorage.getItem('id');
     firebase.database().ref('alumno/'+userId).update({
-        documentoSolitud:notAdd,
+        documentoSolicitud:notAdd,
     }).catch(function(error){
         alert("Su documento no ha sido borrado con éxito. Intentelo mas tarde");
     });
     alert("Documento Eliminado");
     location.reload(); //Actualizar la pagina automáticamente
     document.getElementById('inputSolicitud').value = ""; //Limpia el campo cada que se hace la operación
+}
+
+//******************************************************************************************/
+//Documento = Carta Compromiso
+function actualizarCartaComp(){
+    var userId = localStorage.getItem('id'); //Recuperar el id del alumno
+    var cartaC = document.getElementById('inputCartaCom').value; //Guardar en una variable el enlace del alumno
+    if (cartaC != ""){
+        firebase.database().ref('alumno/'+userId).update({
+            documentoCartaCompromiso:cartaC,
+        }).catch(function(error){
+            alert("Su documento no ha sido guardado con éxito. Intentelo mas tarde");
+        }); 
+        alert("Documento de Evidencia - Guardado");
+        location.reload(); //Actualizar la pagina automáticamente
+        document.getElementById('inputCartaCom').value = ""; //Limpia el campo cada que se hace la operación    
+    }
+}
+
+function eliminarCartaComp(){
+    var userId = localStorage.getItem('id');
+    firebase.database().ref('alumno/'+userId).update({
+        documentoCartaCompromiso:notAdd,
+    }).catch(function(error){
+        alert("Su documento no ha sido borrado con éxito. Intentelo mas tarde");
+    });
+    alert("Documento Eliminado");
+    location.reload(); //Actualizar la pagina automáticamente
+    document.getElementById('inputCartaCom').value = ""; //Limpia el campo cada que se hace la operación
+}
+
+//******************************************************************************************/
+//Documento = Tarjeta de Control
+function actualizarTarjetaCtrl(){
+    var userId = localStorage.getItem('id'); //Recuperar el id del alumno
+    var tarjetaC = document.getElementById('inputTarjetaCtrl').value; //Guardar en una variable el enlace del alumno
+    if (tarjetaC != ""){
+        firebase.database().ref('alumno/'+userId).update({
+            documentoTarjetaCtrl:tarjetaC,
+        }).catch(function(error){
+            alert("Su documento no ha sido guardado con éxito. Intentelo mas tarde");
+        }); 
+        alert("Documento de Evidencia - Guardado");
+        location.reload(); //Actualizar la pagina automáticamente
+        document.getElementById('inputTarjetaCtrl').value = ""; //Limpia el campo cada que se hace la operación
+    }
+}
+
+function eliminarTarjetaCtrl(){
+    var userId = localStorage.getItem('id');
+    firebase.database().ref('alumno/'+userId).update({
+        documentoTarjetaCtrl:notAdd,
+    }).catch(function(error){
+        alert("Su documento no ha sido borrado con éxito. Intentelo mas tarde");
+    });
+    alert("Documento Eliminado");
+    location.reload(); //Actualizar la pagina automáticamente
+    document.getElementById('inputTarjetaCtrl').value = ""; //Limpia el campo cada que se hace la operación
+}
+
+//******************************************************************************************/
+//Documento = Carta Asignación
+function actualizarCartaAsignacion(){
+    var userId = localStorage.getItem('id'); //Recuperar el id del alumno
+    var cartaAs = document.getElementById('inputCartaAsig').value; //Guardar en una variable el enlace del alumno
+    if (cartaAs != ""){
+        firebase.database().ref('alumno/'+userId).update({
+            documentoCartaAsignacion:cartaAs,
+        }).catch(function(error){
+            alert("Su documento no ha sido guardado con éxito. Intentelo mas tarde");
+        }); 
+        alert("Documento de Evidencia - Guardado");
+        location.reload(); //Actualizar la pagina automáticamente
+        document.getElementById('inputCartaAsig').value = ""; //Limpia el campo cada que se hace la operación
+    }
+}
+
+function eliminarCartaAsignacion(){
+    var userId = localStorage.getItem('id');
+    firebase.database().ref('alumno/'+userId).update({
+        documentoCartaAsignacion:notAdd,
+    }).catch(function(error){
+        alert("Su documento no ha sido borrado con éxito. Intentelo mas tarde");
+    });
+    alert("Documento Eliminado");
+    location.reload(); //Actualizar la pagina automáticamente
+    document.getElementById('inputCartaAsig').value = ""; //Limpia el campo cada que se hace la operación
+}
+
+//******************************************************************************************/
+//Documento = Reporte Bimestral
+function actualizarReporteB(){
+    var userId = localStorage.getItem('id'); //Recuperar el id del alumno
+    var repB   = document.getElementById('inputReporteB').value; //Guardar en una variable el enlace del alumno
+    if (repB != ""){
+        firebase.database().ref('alumno/'+userId).update({
+            documentoReporteB:repB,
+        }).catch(function(error){
+            alert("Su documento no ha sido guardado con éxito. Intentelo mas tarde");
+        }); 
+        alert("Documento de Evidencia - Guardado");
+        location.reload(); //Actualizar la pagina automáticamente
+        document.getElementById('inputReporteB').value = ""; //Limpia el campo cada que se hace la operación
+    }
+}
+
+function eliminarReporteB(){
+    var userId = localStorage.getItem('id');
+    firebase.database().ref('alumno/'+userId).update({
+        documentoReporteB:notAdd,
+    }).catch(function(error){
+        alert("Su documento no ha sido borrado con éxito. Intentelo mas tarde");
+    });
+    alert("Documento Eliminado");
+    location.reload(); //Actualizar la pagina automáticamente
+    document.getElementById('inputReporteB').value = ""; //Limpia el campo cada que se hace la operación
+}
+
+//******************************************************************************************/
+//Documento = Formato Evaluacion
+function actualizarEvaluacion(){
+    var userId = localStorage.getItem('id'); //Recuperar el id del alumno
+    var eva    = document.getElementById('inputEvaluacion').value; //Guardar en una variable el enlace del alumno
+    if (eva != ""){
+        firebase.database().ref('alumno/'+userId).update({
+            documentoEvaluacion:eva,
+        }).catch(function(error){
+            alert("Su documento no ha sido guardado con éxito. Intentelo mas tarde");
+        }); 
+        alert("Documento de Evidencia - Guardado");
+        location.reload(); //Actualizar la pagina automáticamente
+        document.getElementById('inputEvaluacion').value = ""; //Limpia el campo cada que se hace la operación
+    }
+}
+
+function eliminarEvaluacion(){
+    var userId = localStorage.getItem('id');
+    firebase.database().ref('alumno/'+userId).update({
+        documentoEvaluacion:notAdd,
+    }).catch(function(error){
+        alert("Su documento no ha sido borrado con éxito. Intentelo mas tarde");
+    });
+    alert("Documento Eliminado");
+    location.reload(); //Actualizar la pagina automáticamente
+    document.getElementById('inputEvaluacion').value = ""; //Limpia el campo cada que se hace la operación
 }
